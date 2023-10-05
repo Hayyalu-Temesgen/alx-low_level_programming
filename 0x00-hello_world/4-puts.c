bb41540f5cf0bfa5062be13ee2403290e8ee183d
Hayyalu Temesgen <hayyalutm@gmail.com>
2023-10-05 08:50:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - this is a main function
- * Return 0 - this will return 0
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a mutilingual puzzle");
+	puts("\"Programming is like building a mutilingual puzzle ");
 	return (0);
 }
