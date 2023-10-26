cc69a91f1588f30e0a679dd2a02f770031c90677
Hayyalu Temesgen <hayyalutm@gmail.com>
2023-10-26 13:00:02 -0700

    102-fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first Fibonacci numbers
+ * Return: alwasy 0
+ */
+int main(void)
+{
+	long int i, j, k, n;
+
+	j = 1;
+	k = 2;
+
+	for (i = 1; i <= 50; ++i)
+	{
+		if (j != 20364011074)
+		{
+			printf("%ld, ", j);
+		}
+		else
+		{
+			printf("%ld\n", j);
+		}
+		n = j + k;
+		j = k;
+		k = n;
+	}
+	return (0);
+}
