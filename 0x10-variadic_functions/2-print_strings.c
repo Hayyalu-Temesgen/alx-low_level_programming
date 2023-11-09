011153aa6873db28132cc9e0d1b723036b5cf060
Hayyalu Temesgen <hayyalutm@gmail.com>
2023-11-09 12:17:05 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		s = va_arg(str, char *);
 		if (s == NULL)
-			printf("null");
+			printf("(nil)");
 		else
 			printf("%s", s);
 
